b31052da4866faf8c859ac9681052cd14168cfb2
Julian-Cape <bravedigit@gmail.com>
2017-07-23 21:06:56 +0200

    Add files via upload

---
+++ mobile_src/Chat/app/src/main/java/giuli/chat/serverReceiveDataTask.java
@@ -0,0 +1,99 @@
+package giuli.chat;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class serverReceiveDataTask implements Runnable{
+
+    HttpURLConnection urlConnection;
+    String dataFromServer = "";
+
+    @Override
+    public void run() {
+
+        try
+        {
+            while(true)
+            {
+
+                try {
+                    URL url = new URL("http://www.bravedigit.com/php/mobilechat_in.php");
+
+                    urlConnection = (HttpURLConnection) url.openConnection();
+
+                    urlConnection.setReadTimeout(10000);
+                    urlConnection.setChunkedStreamingMode(0);
+
+                    InputStream inputStreamChat = new BufferedInputStream(urlConnection.getInputStream());
+                    dataFromServer = readStream(inputStreamChat);
+
+                    if(!dataFromServer.equals("")) {
+
+                        //readReceivedText = (TextView) findViewById(R.id.textReceived);
+
+                        Chat.dataFromServer_global += "\nCLIENT - ";
+                        Chat.dataFromServer_global += dataFromServer;
+
+                        //Thread updateReceivedText = new Thread(new runUpdateReceivedText());
+                        //updateReceivedText.start();
+
+                        try {
+                            Chat.readReceivedText.setText(Chat.dataFromServer_global);
+                        }
+                        catch(Exception e)
+                        {
+                            e.printStackTrace();
+                        }
+
+
+
+                    }
+
+                    urlConnection.disconnect();
+                }
+                catch (IOException except)
+                {
+                    except.printStackTrace();
+                }
+                catch (Exception except)
+                {
+                    except.printStackTrace();
+                }
+
+                Thread.sleep(4000);
+
+            }
+
+        }
+        catch (InterruptedException except) {
+            except.printStackTrace();
+        }
+
+    }
+
+//    private class runUpdateReceivedText implements Runnable{
+//
+//            @Override
+//            public void run() {
+//                Chat.readReceivedText.setText(Chat.dataFromServer_global);
+//            }
+//
+//    }
+    private String readStream(InputStream is) {
+        try {
+            ByteArrayOutputStream bo = new ByteArrayOutputStream();
+            int i = is.read();
+            while(i != -1) {
+                bo.write(i);
+                i = is.read();
+            }
+            return bo.toString();
+        } catch (IOException e) {
+            return "";
+        }
+    }
+}
