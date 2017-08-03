4e19046a8abd1e82456dc5acc65dbfa030dcf5b4
Julian-Cape <bravedigit@gmail.com>
2017-08-03 20:23:53 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<!DOCTYPE html>
+<head>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
     <?php   
 
 
@@ -159,4 +167,7 @@
    <?php if($chat_already_on == false){echo 'window.onbeforeunload = closingCode;';}?> 
  
 
-</script>
+  </script>
+    
+</body>
+</html>     
